37aab40121de3114c174c39b8d03fcb328307c8e
NIRBHAY NARAYAN SINGH <44487887+nns15899@users.noreply.github.com>
2018-10-30 21:19:23 +0530

    Add files via upload

---
+++ multiplication table.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	int num,i;
+	printf("enter any number::");
+	scanf("%d",&num);
+	for(i=1;i<=10;i++){
+		int result=num*i;
+		printf("\n%d * %d=%d",num,i,result);
+	
+	}
+	                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
+}
+
